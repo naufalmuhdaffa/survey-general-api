0b5ebeb175a693e3e2b2af445da91b67cd08782d
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-05-09 07:39:44 +0700

    feat(helpers): add JSON response helper

---
+++ src/Helpers/Response.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+final class Response
+{
+    /**
+     * @param mixed $data
+     * @param array<string, string> $headers
+     */
+    public static function json($data, int $code = 200, array $headers = []): never
+    {
+        http_response_code($code);
+        header('Content-Type: application/json');
+
+        foreach ($headers as $key => $value) {
+            header("{$key}: {$value}");
+        }
+
+        echo json_encode(
+            $data,
+            JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES,
+        );
+
+        exit;
+    }
+}
