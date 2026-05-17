30027b3ef7420d079c310ab59d19a6bc07b64135
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-05-17 13:08:21 +0700

    feat(survey): add routes for answer option

---
+++ src/Features/Survey/Question/Option/OptionRoutes.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Features\Survey\Question\Option;
+
+use App\Interfaces\RoutesInterface;
+use App\Features\Survey\Question\Option\Create\CreateOptionRoutes;
+
+final class OptionRoutes implements RoutesInterface
+{
+    public static function dispatch(string $path, string $method, array $segments): bool
+    {
+        if (CreateOptionRoutes::dispatch($path, $method, $segments)) {
+            return true;
+        }
+
+        return false;
+    }
+}
--- src/Features/Survey/Question/QuestionRoutes.php
+++ src/Features/Survey/Question/QuestionRoutes.php
@@ -6,12 +6,17 @@ namespace App\Features\Survey\Question;
 
 use App\Interfaces\RoutesInterface;
 use App\Features\Survey\Question\Create\CreateQuestionRoutes;
+use App\Features\Survey\Question\Option\OptionRoutes;
 
 final class QuestionRoutes implements RoutesInterface
 {
     public static function dispatch(string $path, string $method, array $segments): bool
     {
         if (CreateQuestionRoutes::dispatch($path, $method, $segments)) {
+            return true;
+        }
+
+        if (OptionRoutes::dispatch($path, $method, $segments)) {
             return true;
         }
 
