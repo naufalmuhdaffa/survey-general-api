c0ae585906196ff6ab0db937b39dacd74805b00b
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-05-17 14:10:18 +0700

    feat(survey): add controller for detail

---
+++ src/Features/Survey/Detail/DetailSurveyController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Features\Survey\Detail;
+
+use App\Helpers\Response;
+
+final class DetailSurveyController
+{
+    private DetailSurveyRepository $repository;
+
+    public function __construct()
+    {
+        $this->repository = new DetailSurveyRepository();
+    }
+
+    public function detail(int $surveyId): void
+    {
+        $survey = $this->repository->getSurveyById($surveyId);
+
+        if (!$survey) {
+            Response::json([
+                'status' => 'error',
+                'message' => 'Survei tidak ditemukan'
+            ], 404);
+        }
+
+        $questions = $this->repository->getQuestionsBySurveyId($surveyId);
+
+        foreach ($questions as &$question) {
+            $question['options'] = $this->repository->getOptionsByQuestionId($question['id']);
+        }
+
+        $survey['questions'] = $questions;
+
+        Response::json([
+            'status' => 'success',
+            'data' => $survey
+        ]);
+    }
+}
