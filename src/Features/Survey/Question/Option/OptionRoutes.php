177f7ce5b2acb1606009838f01a7c75ec6de975c
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-05-19 05:33:09 +0700

    feat(survey): add routes for update option

--- src/Features/Survey/Question/Option/OptionRoutes.php
+++ src/Features/Survey/Question/Option/OptionRoutes.php
@@ -6,12 +6,17 @@ namespace App\Features\Survey\Question\Option;
 
 use App\Interfaces\RoutesInterface;
 use App\Features\Survey\Question\Option\Create\CreateOptionRoutes;
+use App\Features\Survey\Question\Option\Update\UpdateOptionRoutes;
 
 final class OptionRoutes implements RoutesInterface
 {
     public static function dispatch(string $path, string $method, array $segments): bool
     {
         if (CreateOptionRoutes::dispatch($path, $method, $segments)) {
+            return true;
+        }
+
+        if (UpdateOptionRoutes::dispatch($path, $method, $segments)) {
             return true;
         }
 
