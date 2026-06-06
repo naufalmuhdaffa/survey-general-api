8b3535a3b8d0e292971c3e76f4916a8ad828a307
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-06-06 08:20:04 +0700

    refactor(user): add route for update role

--- src/Features/User/UserRoute.php
+++ src/Features/User/UserRoute.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace App\Features\User;
 
 use App\Features\User\List\ListUserRoute;
-use App\Features\User\Permission\UserPermissionRoute;
 use App\Features\User\Promotable\ListPromotableUserRoute;
-use App\Features\User\Promote\PromoteUserRoute;
+use App\Features\User\Role\UpdateUserRoleRoute;
 use App\Features\User\Status\UpdateUserStatusRoute;
 use App\Interfaces\RouteInterface;
 
@@ -23,15 +22,11 @@ final class UserRoute implements RouteInterface
             return true;
         }
 
-        if (PromoteUserRoute::dispatch($path, $method, $segments)) {
+        if (UpdateUserRoleRoute::dispatch($path, $method, $segments)) {
             return true;
         }
 
         if (UpdateUserStatusRoute::dispatch($path, $method, $segments)) {
-            return true;
-        }
-
-        if (UserPermissionRoute::dispatch($path, $method, $segments)) {
             return true;
         }
 
